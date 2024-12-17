5c021b56e7538e71465519fc9fda04f23ba2f5de
astro-israfil <153127727+astro-israfil@users.noreply.github.com>
2024-12-17 16:19:14 +0000

    variables in javaScript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 123454
+let accountEmail = "im.fi@google.com"
+var accountPassword = "12345"
+accountCity = "Dhaka"
+
+let accountState;
+
+
+// reasigning variables value
+
+// accountId = 23445 // TypeError: Assignment to constant variable.
+
+accountEmail = "israfil@google.com" 
+accountPassword = "8966"
+accountCity = "Manikganj"
+console.log(accountId)
+
+console.table({accountEmail, accountPassword, accountCity, accountState})
