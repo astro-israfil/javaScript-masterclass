87572255bdabb277c768cdc7423fdd57d3512e55
astro-israfil <astro.israfil.fi@gmail.com>
2024-12-17 22:51:31 +0600

    data types in javaScript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all code below as newer version of javaScript
+
+// alert("Hello World!") // we are running our codes in nodejs not in browser, here is alert function is not defined
+
+
+// Data Types
+let name = "Israfil" // string
+let myBigNumber = BigInt(1234567890123456) // bigint
+let age = 22 // number
+let isLoggedIn = true // boolean
+let status = null // a standalone value, typeof functions return object for null
+let address; // undefined
+let mySymbol = Symbol("123") // symbol
+
+let person = { firstName: "Israfil", age: 22} // object
+
+console.log(typeof status)
