838465fac2fe5b3f1b482d2b7b803da2892edf5d
astro-israfil <astro.israfil.fi@gmail.com>
2024-12-17 23:20:48 +0600

    type conversion in javaScript

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = " 0 12"
+
+let valueInNumber = Number(score)
+
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// conversion
+// "33" => 33
+// "33abc" => NaN
+// "" => 0
+// " " => 0
+// null => 0
+// undefined => NaN
+// true => 1; false => 0
+
+const isLoggedIn = undefined
+const booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false; " " => true
+// null => false 
+// undefined => false
+
+const status = undefined
+const statusInString = String(status)
+
+console.log(statusInString)
+console.log(typeof statusInString)
