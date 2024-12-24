c80fc19139f6ac82f8b384680f851100ebc2d293
astro-israfil <astro.israfil.fi@gmail.com>
2024-12-24 21:37:13 +0600

    variables scopes and hoisting in javaScript

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -14,6 +14,51 @@ if (true) {
     var c = 30 // it changes the value of the global variable c
 }
 
-console.log("global a:",a)
-console.log("global b:", b)
-console.log(c) // 30
+// console.log("global a:",a)
+// console.log("global b:", b)
+// console.log(c) // 30
+
+
+
+
+function one() {
+    const username = "Israfil"
+
+    function two() {
+        const website = "israfil.com"
+        console.log(username)
+    }
+
+    // console.log(website) // ReferenceError: website is not defined
+    two()
+}
+
+// one()
+
+
+// if (true) {
+//     const name = "Israfil"
+//     if (name === "Israfil") {
+//         const age = 22
+//         console.log(`My name is ${name} and I am ${age} years old`)
+//     }
+
+//     // console.log(age) // ReferenceError: age is not defined
+// }
+
+// console.log(name)
+
+
+
+/////////////////////////// HOISTING ///////////////////////////
+
+// myFunction1()
+
+// function myFunction1() {
+//     console.log("Hello from myFunction1")
+// }
+
+// myFunction2()
+// const myFunction2 = function() {
+//     console.log("Hello from myFunction2")
+// }
