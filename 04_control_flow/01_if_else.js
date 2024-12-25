a3d23db91ca51bf5fe90e25c074d7eef2eb82752
astro-israfil <astro.israfil.fi@gmail.com>
2024-12-25 10:43:53 +0600

    if else statement in javaScript

---
+++ 04_control_flow/01_if_else.js
@@ -0,0 +1,61 @@
+const username = 'alice';
+
+// if (username !== undefined) {
+//   console.log(`Hello ${username}`);
+// }
+
+// if (username) {
+//   console.log(`Hello ${username}`);
+// }
+
+const userLoggedIn = true;
+
+// if (userLoggedIn) {
+//   console.log('User is logged in');
+// } else {
+//   console.log('User is not logged in');
+// }
+
+
+// const age = 18;
+
+// if (age >= 18) {
+//     console.log('You are an adult');
+// } else {
+//     console.log('You are a minor');
+// }
+
+
+const userSignedInWithGoogle = false;
+const userSignedInWithFacebook = true;
+
+// if (userSignedInWithGoogle || userSignedInWithFacebook) {
+//     console.log('User is signed in');
+// } else {
+//     console.log('User is not signed in');
+// }
+
+
+// const isPremiumUser = true;
+// if (userLoggedIn && isPremiumUser) {
+//     console.log('User is a premium user and can access all features');
+// } else if (userLoggedIn) {
+//     console.log('User is logged in and can access limited features');
+// } else {
+//     console.log('User is not logged in');
+// }
+
+
+const marks = 95;
+
+if (marks >= 90) {
+    console.log('Grade A'); 
+} else if (marks >= 80) {
+    console.log('Grade B');
+} else if (marks >= 70) {
+    console.log('Grade C');
+} else if (marks >= 60) {
+    console.log('Grade D');
+} else {
+    console.log('Grade F');
+}
