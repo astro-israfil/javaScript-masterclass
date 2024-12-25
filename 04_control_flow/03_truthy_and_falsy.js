316b965a9cf4e4843b746051f54fa109a8706ac0
astro-israfil <astro.israfil.fi@gmail.com>
2024-12-25 11:17:23 +0600

    switch and truthy falsy value in javaScript

---
+++ 04_control_flow/03_truthy_and_falsy.js
@@ -0,0 +1,35 @@
+const username = NaN;
+
+if (username) {
+    console.log("The username is truthy");
+}
+
+// falsy values: 0, -0, NaN, "", null, undefined, false
+
+// truthy values: everything else
+// {}, [], "0", "false", "null", "undefined", true, 1, -1, Infinity, -Infinity, function() {}
+
+
+// Nullish Coalescing Operator (??)
+
+const temperature = null
+const defaultTemperature = 0 ?? null;
+
+console.log(defaultTemperature); // 0
+
+
+// ternary operator
+
+const age = 18;
+const canGetADriverLicense = age >= 18 ? "Yes" : "No";
+
+console.log(canGetADriverLicense); // Yes
+
+
+age >= 18 ? 
+    console.log("You can get a driver license") 
+    : console.log("You can't get a driver license");
+
+// short-circuiting
+const displayName = username || "Guest";
+console.log(displayName); // Guest
